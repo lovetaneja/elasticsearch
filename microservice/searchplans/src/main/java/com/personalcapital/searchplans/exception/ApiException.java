fcc68384381f85c0a0bcaeb045bfa1d4d69a6ed3
lovetaneja <taneja.love@gmail.com>
2019-02-23 23:05:17 -0800

    Create ApiException.java
    Exception class

---
+++ microservice/searchplans/src/main/java/com/personalcapital/searchplans/exception/ApiException.java
@@ -0,0 +1,19 @@
+package com.personalcapital.searchplans.exception;
+
+/**
+ * @author love_taneja
+ * This is exception class for any API exception
+ */
+public class ApiException extends Exception {
+
+    private int code;
+
+    public ApiException(int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+}
